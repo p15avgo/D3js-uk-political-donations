182447a248b80b637b1aaf903150180b96db60f5
AndreasAugoustis <p15avgo@ionio.gr>
2018-03-13 00:05:13 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -277,11 +277,7 @@ function moveToFunds(alpha) {
 function moveToAmounts(alpha) {
 	return function(d) {
 		var centreY = svgCentre.y;
-			if (d.value <= 50001) {
-				centreX = svgCentre.x + 600;
-				centreY = svgCentre.y + 50;//500
-			}
-			else if (d.value <= 100001) {
+			if (d.value <= 100001) {
 				centreX = svgCentre.x + 600;
 				centreY = svgCentre.y + 75;//500
 			} else if (d.value <= 250001) {
